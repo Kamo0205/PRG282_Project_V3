ff98f4a1026d19d64ea555901db6dffee269af2f
GitKhwezi <56929788+GitKhwezi@users.noreply.github.com>
2019-10-23 20:37:37 +0200

    Update Email.cs

--- PRG282_Project/Email.cs
+++ PRG282_Project/Email.cs
@@ -2,14 +2,62 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
+using System.Net.Mail;
 
-namespace PRG282_Project
+namespace EmailPractice
 {
-    class Email
+    public class Email
     {
-        public void SendEmail(string text)
+        private string message;
+
+        public string Message
         {
-            
+            get { return message; }
+            set { message = value; }
+        }
+
+        private string sendTo;
+
+        public string SendTo
+        {
+            get { return sendTo; }
+            set { sendTo = value; }
+        }
+
+        private string subject;
+
+        public string Subject
+        {
+            get { return subject; }
+            set { subject = value; }
+        }
+
+        public void SendEmail(string message)
+        {
+            try
+            {
+                MailMessage mail = new MailMessage();
+                SmtpClient SmtpServer = new SmtpClient("smtp.mail.yahoo.com");
+
+                mail.From = new MailAddress("m.khwezi@yahoo.com");
+                mail.To.Add("jannesventer09@gmail.com");
+                mail.Subject = "Testing Email";
+                mail.Body = message;
+
+                SmtpServer.Port = 587;
+                SmtpServer.Credentials = new System.Net.NetworkCredential("m.khwezi@yahoo.com", "YaWena1997");
+                SmtpServer.EnableSsl = true;
+
+                SmtpServer.Send(mail);
+                Console.WriteLine("Mail Sent"); 
+
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
         }
     }
 }
