7fda928301c4a40f9aa8b1374715dc902c41dbf7
jannes09 <jannesventer09@gmail.com>
2019-10-23 17:16:10 +0200

    Added Controller class
    Added controller class that calls datatables from the datahandler and changed datahandler to return datatables.

--- PRG282_Project/DataHandler.cs
+++ PRG282_Project/DataHandler.cs
@@ -8,16 +8,31 @@ using System.Data.SqlClient;
 
 namespace PRG282_Project
 {
+    delegate int GetRankID(string rank);
     class DataHandler
     {
         SqlConnection conn;
+        GetRankID getRankID;
 
         public DataHandler()
         {
             conn = new SqlConnection(ConfigurationManager.ConnectionStrings["cstring"].ConnectionString);
+            Controller cn = new Controller();
+            getRankID = new GetRankID((string rank) =>
+            {
+                int targetRank = 0;
+                foreach (string[] item in cn.ReadRanks())
+                {
+                    if (rank.ToLower() == item[1].ToLower())
+                    {
+                        targetRank = int.Parse(item[0]);
+                    }
+                }
+                return targetRank;
+            });
         }
 
-        public  List<Person> ReadPeople()
+        public DataTable GetPeople()
         {
             DataTable result = new DataTable();
             try
@@ -44,15 +59,123 @@ namespace PRG282_Project
                     conn.Close();
                 }
             }
-            List<Person> temp = new List<Person>();
-            foreach (DataRow row in result.Rows)
-            {
-                temp.Add(new Person(row["IDNumber"].ToString(), row["Name"].ToString(), row["Surname"].ToString(), int.Parse(row["Age"].ToString()),row["Title"].ToString(), row["Password"].ToString()));
-            }
-            return temp;
+
+            return result;
         }
 
-        public List<Message> ReadMessages()
+        public int AddPerson(Person person)
+        {
+            int result = 0;
+            try
+            {
+                if (conn.State != ConnectionState.Open)
+                {
+                    conn.Open();
+
+                    string query = string.Format(@"Begin Try
+	Begin Transaction
+		Insert Into [tblUsers]([Name],[Surname],[Age],[RankID],[Username],[Password])
+		Values('{0}','{1}',{2},{3},'{4}','{5}')
+	Commit Transaction
+End Try
+Begin Catch
+	Rollback
+End Catch", person.Name, person.Surname, person.Age, getRankID(person.Rank), person.Username, person.Password);
+
+                    SqlCommand cmd = new SqlCommand(query, conn);
+                    result = cmd.ExecuteNonQuery();
+                }
+            }
+            catch (SqlException SqlEx)
+            {
+                Console.WriteLine(SqlEx.Message); ;
+            }
+            finally
+            {
+                if (conn.State == ConnectionState.Open)
+                {
+                    conn.Close();
+                }
+            }
+            return result;
+        }
+
+        public int DeletePerson(Person person)
+        {
+            int result = 0;
+            try
+            {
+                if (conn.State != ConnectionState.Open)
+                {
+                    conn.Open();
+
+                    string query = string.Format(@"Begin Try
+	Begin Transaction
+		Delete From [tblUsers]
+        Where [Name] = '{0}' And [Surname] = '{1}'
+	Commit Transaction
+End Try
+Begin Catch
+	Rollback
+End Catch", person.Name, person.Surname);
+
+                    SqlCommand cmd = new SqlCommand(query, conn);
+                    result = cmd.ExecuteNonQuery();
+                }
+            }
+            catch (SqlException SqlEx)
+            {
+                Console.WriteLine(SqlEx.Message); ;
+            }
+            finally
+            {
+                if (conn.State == ConnectionState.Open)
+                {
+                    conn.Close();
+                }
+            }
+            return result;
+        }
+
+        public int UpdatePerson(Person oldPerson, Person newPerson)
+        {
+            int result = 0;
+            try
+            {
+                if (conn.State != ConnectionState.Open)
+                {
+                    conn.Open();
+
+                    string query = string.Format(@"Begin Try
+	Begin Transaction
+		Update [tblUsers]
+        set [Name] = '{0}',[Surname] = '{1}',[Age] = {2},[RankID] = {3},[Username] = '{4}',[Password] = '{5}'
+        Where [Name] = '{6}' And [Surname] = '{7}'
+	Commit Transaction
+End Try
+Begin Catch
+	Rollback
+End Catch", oldPerson.Name, oldPerson.Surname, oldPerson, oldPerson.Age, getRankID(oldPerson.Rank), oldPerson.Password, newPerson.Name, newPerson.Surname);
+
+                    SqlCommand cmd = new SqlCommand(query, conn);
+                    result = cmd.ExecuteNonQuery();
+                }
+            }
+            catch (SqlException SqlEx)
+            {
+                Console.WriteLine(SqlEx.Message); ;
+            }
+            finally
+            {
+                if (conn.State == ConnectionState.Open)
+                {
+                    conn.Close();
+                }
+            }
+            return result;
+        }
+
+        public DataTable GetMessages()
         {
             DataTable result = new DataTable();
             try
@@ -79,15 +202,10 @@ namespace PRG282_Project
                     conn.Close();
                 }
             }
-            List<Message> temp = new List<Message>();
-            foreach (DataRow row in result.Rows)
-            {
-                temp.Add(new Message(new Person("", row["Name"].ToString(), row["Surname"].ToString(), 0, "", ""),row["Content"].ToString()));
-            }
-            return temp;
+            return result;
         }
 
-        public List<string[]> ReadLogins()
+        public DataTable GetLogins()
         {
             DataTable result = new DataTable();
             try
@@ -114,13 +232,39 @@ namespace PRG282_Project
                     conn.Close();
                 }
             }
-            List<string[]> temp = new List<string[]>();
-            foreach (DataRow row in result.Rows)
+            
+            return result;
+        }
+
+        public DataTable GetRanks()
+        {
+            DataTable result = new DataTable();
+            try
             {
-                string[] str = new string[] { row["Username"].ToString(), row["Password"].ToString() };
-                temp.Add(str);
+                if (conn.State != ConnectionState.Open)
+                {
+                    conn.Open();
+
+                    string query = @"Select * From [tblRanks]";
+
+                    SqlDataAdapter adapter = new SqlDataAdapter(query, conn);
+
+                    adapter.Fill(result);
+                }
             }
-            return temp;
+            catch (SqlException SqlEx)
+            {
+                Console.WriteLine(SqlEx.Message); ;
+            }
+            finally
+            {
+                if (conn.State == ConnectionState.Open)
+                {
+                    conn.Close();
+                }
+            }
+            
+            return result;
         }
     }
 }
